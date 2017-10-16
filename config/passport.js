6079d51e56fb4185385ca60a13044386676aa2ea
Rajesh D <dhrajesh1@gmail.com>
2017-10-16 16:43:47 +0530

    passport -2
    passport -2

--- config/passport.js
+++ config/passport.js
@@ -10,11 +10,25 @@ module.exports = function(passport){
     passport.use(new LocalStrategy({usernameField:'email'},
         (email,password,done) => {
             // console.log(`User Email :: ${email}, User Password :: ${password}`)
+            console.log(email)
             User.findOne({
                 email:email
-            }).then((err,user) => {
+            }).then((user) => {
+                console.log(user)
                 if(user == null){
                     return done(null,false,{'message':'No user found'});
+                }else{
+                    //match the password
+                    bcrypt.compare(password,user.password,(err,isMatched) => {
+                        if(err){
+                            throw err
+                        }
+                        if(isMatched){
+                            return done(null,user)
+                        }else{
+                            return done(null,false,{'message':'Password incorrect'});
+                        }
+                    })
                 }
             })
         }
